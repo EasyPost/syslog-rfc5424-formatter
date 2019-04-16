99016e78ba056cb00024ec85bc94baf11193ee5c
James Brown <jbrown@easypost.com>
2019-04-15 17:36:47 -0700

    add project_urls to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setup(
     py_modules=['syslog_rfc5424_formatter'],
     keywords=["logging"],
     description="Logging formatter which produces well-formatted RFC5424 Syslog Protocol messages",
+    project_urls={
+        'Issue Tracker': 'https://github.com/easypost/syslog-rfc5424-formatter/issues',
+        'Documentation': 'https://syslog-rfc5424-formatter.readthedocs.io/en/latest/',
+    },
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     classifiers=[
