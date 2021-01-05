be8518969ea4c346d0ffd8d851b1f00ee467b019
James Brown <jbrown@easypost.com>
2021-01-04 17:17:00 -0800

    drop support for python 3.3, 3.4, and 3.5

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     },
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
